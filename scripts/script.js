f1483cc6e1867d1904b0a30ec3c716451c547068
JokerFelix <HerrnFelixBecker@gmail.com>
2025-10-30 18:56:09 +0100

    Added todos for dynamic gallery.

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,24 @@
-// Experimental function to calculated total cost of services.
-function calculateTotal() {
-    const checkboxes = document.querySelectorAll('input[name="service"]:checked');
-    let total = 0;
-    checkboxes.forEach((checkbox) => {
-        total += parseFloat(checkbox.value);
-    });
-    document.getElementById('total-cost').textContent = `Total Cost: $${total}`;
-}
-
 // Enables the Anfang button if scrolled down a bit.
-window.onscroll = function() {
+function enableToTopButton() {
     const scrollTopBtn = document.getElementById("scrollTopBtn");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         scrollTopBtn.style.display = "block";
     } else {
         scrollTopBtn.style.display = "none";
     }
-};
+}
+
+// Binds the Anfang button display to onScroll event.
+window.addEventListener("scroll", enableToTopButton);
 
 // Scroll to Top when using this function.
 function scrollToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
+
+// TODO: Function switching to next image displayed in the gallery.
+
+// TODO: Function switching to previous image displayed in the gallery.
+
+// TODO: Add timer event with next image as callback to automatically change images after x seconds.
