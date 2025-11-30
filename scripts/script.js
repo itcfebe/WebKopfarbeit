0f38d629c74e0227df6d7ef94bc89d155285df37
JokerFelix <HerrnFelixBecker@gmail.com>
2025-11-30 15:12:09 +0100

    Formatted css and js as well.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 // Enables the Anfang button if scrolled down a bit.
 function enableToTopButton() {
-    const scrollTopBtn = document.getElementById("scrollTopBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollTopBtn.style.display = "block";
-    } else {
-        scrollTopBtn.style.display = "none";
-    }
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
 }
 
 // Binds the Anfang button display to onScroll event.
@@ -13,8 +13,8 @@ window.addEventListener("scroll", enableToTopButton);
 
 // Scroll to Top when using this function.
 function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
 // TODO: Function switching to next image displayed in the gallery.
 
